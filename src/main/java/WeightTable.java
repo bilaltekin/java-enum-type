f02f93131f94f0e2b4905431069648a4436b1ad1
Bilal Tekin <bt.ce21@gmail.com>
2020-11-25 11:19:13 +0300

    first commit

---
+++ src/main/java/WeightTable.java
@@ -0,0 +1,15 @@
+public class WeightTable {
+    public static void main(String[] args) {
+        double earthWeight = Double.parseDouble("10");
+        double mass = earthWeight / Planet.EARTH.surfaceGravity();
+
+
+        for (Planet p : Planet.values())
+        {
+            System.out.printf("Weight on %s is %f%n \n",
+                    p, p.surfaceWeight(mass));
+        }
+
+
+    }
+}
